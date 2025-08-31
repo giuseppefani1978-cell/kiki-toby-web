d2788be38015c7a742db7d64df9e9cda39b19cc3
giuseppefani1978-cell <giuseppe.fani1978@gmail.com>
2025-08-31 16:36:13 +0200

    Create MapView.tsx

---
+++ src/components/MapView.tsx
@@ -0,0 +1,60 @@
+import React, { useMemo, useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import poiData from '../data/poi/05_quartier_latin.json';
+import partners from '../data/partners/cafes_rive_gauche.json';
+import DialogueCard from './DialogueCard';
+
+type POI = typeof poiData[number];
+
+const iconBlue = new L.Icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  iconSize: [25,41], iconAnchor:[12,41], popupAnchor:[1,-34]
+});
+const iconBrown = new L.Icon({
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconSize: [25,41], iconAnchor:[12,41], popupAnchor:[1,-34]
+});
+
+export default function MapView() {
+  const [focus, setFocus] = useState<POI|null>(null);
+  const pois = useMemo(() => poiData, []);
+
+  // Optionnel : centrage sur l’utilisateur (si autorisé)
+  const [center, setCenter] = useState<[number, number]>([48.8465, 2.344]);
+  useEffect(() => {
+    if (!('geolocation' in navigator)) return;
+    const id = navigator.geolocation.watchPosition(
+      (pos) => setCenter([pos.coords.latitude, pos.coords.longitude]),
+      () => {},
+      { enableHighAccuracy: true, maximumAge: 5000, timeout: 10000 }
+    );
+    return () => navigator.geolocation.clearWatch(id);
+  }, []);
+
+  return (
+    <>
+      <MapContainer id="map" center={center} zoom={15} preferCanvas touchZoom inertia={false}>
+        <TileLayer
+          attribution='&copy; OpenStreetMap'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {pois.map(p => (
+          <Marker key={p.id} position={[p.lat, p.lng]} icon={iconBlue}
+                  eventHandlers={{ click: () => setFocus(p) }}>
+            <Popup>{p.title}</Popup>
+          </Marker>
+        ))}
+        {partners.map(pt => (
+          <Marker key={pt.id} position={[pt.lat, pt.lng]} icon={iconBrown}>
+            <Popup>{pt.name} (Partenaire)</Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+
+      {focus && (
+        <DialogueCard kiki={focus.kiki!} toby={focus.toby!} poiId={focus.id} />
+      )}
+    </>
+  );
+}
