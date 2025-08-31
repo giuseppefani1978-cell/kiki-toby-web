fb5708795fabd2b2cf0e7b82dc119ff97cab8dce
giuseppefani1978-cell <giuseppe.fani1978@gmail.com>
2025-08-31 22:50:20 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './Apps'; // ← ou renomme le fichier en App.tsx et importe './App'
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
