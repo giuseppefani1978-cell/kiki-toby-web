2b9377bdfec001cdd7672ecbabfa038f96da3d59
giuseppefani1978-cell <giuseppe.fani1978@gmail.com>
2025-09-05 16:33:42 +0000

    chore: drop .js deplicate (keep TS/TSX only)

--- src/store/game.js
+++
@@ -1,8 +0,0 @@
-export const game = { moustaches: 0, pattes: 0, album: [] };
-export function addFragment(f) {
-    game.album.push(f);
-    if (f.who === 'kiki')
-        game.moustaches += 1;
-    else
-        game.pattes += 1;
-}
